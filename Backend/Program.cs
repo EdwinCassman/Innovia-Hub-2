68f7f44f79967daf853beb76438054f569fdbc94
Edwin Cassman <132938446+EdwinCassman@users.noreply.github.com>
2025-10-31 17:13:26 +0100

    Update CORS policy to allow specific origin

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -29,8 +29,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", 
-    "https://orca-app-ulof5.ondigitalocean.app") // Frontendens URL
+        policy.WithOrigins("https://orca-app-ulof5.ondigitalocean.app") // Frontendens URL
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials(); // Viktigt för SignalR
